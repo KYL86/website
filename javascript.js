265d984d23dea80476a4a9f029c13a2df8157b7c
KYL86 <mayjack86@gmail.com>
2019-10-13 23:05:02 +1300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -32,14 +32,55 @@ function showIt(){
 	document.getElementById('mycart').style="background:white;border:1px solid #CCCCCC;border-bottom:2px solid white;height:30px;z-index:2;";
 	
 
-}
+};
 
 function hiddenIt(){
 	document.getElementById('minicart').style.display="none";
 	document.getElementById('mycart').style="background:none;border:none;";
 	
 
-}
+};
+
+function addToCart(){
+	var size= document.getElementById('size1').innerHTML;
+	
+
+	
+	if (size=="S") {
+		var sgetQuantity= parseInt(document.getElementById('squantity').innerHTML);
+		
+
+		document.getElementById('small').style="display:block;";
+		document.getElementById('minicart').style="display:block;";
+		document.getElementById('empty').style="display:none";
+		document.getElementById('squantity').innerHTML=sgetQuantity+1;
+
+	} else if (size=="M") {
+		var mgetQuantity= parseInt(document.getElementById('mquantity').innerHTML);
+		
+
+		document.getElementById('medium').style="display:block;";
+		document.getElementById('minicart').style="display:block;";
+		document.getElementById('empty').style="display:none";
+		document.getElementById('mquantity').innerHTML=mgetQuantity+1;
+		
+		
+	}
+	else if (size=="L") {
+		var lgetQuantity= parseInt(document.getElementById('lquantity').innerHTML);
+		
+
+		document.getElementById('large').style="display:block;";
+		document.getElementById('minicart').style="display:block;";
+		document.getElementById('empty').style="display:none";
+		document.getElementById('lquantity').innerHTML=lgetQuantity+1;
+		
+	}else if(size==""){
+		document.getElementById('highlight').style="-moz-animation:hlight 1s;-webkit-animation:hlight 1s;-o-animation:myfirst 1s;height:20px;width:30px;"}
+
+	};
+	
+
 
 
 
